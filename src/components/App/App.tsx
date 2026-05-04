1ff4512ece7357ab272efd671acac3f57d7e50b9
Maksim <70702008m@gmail.com>
2026-05-04 21:05:40 +0300

    text

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+
 export default function App() {
-    return (
-        <h1>Helloo</h1>
-    )
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["character"],
+    queryFn: async () => {
+      const res = await fetch("https://rickandmortyapi.com/api/character");
+      return res.json();
+    },
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error...</p>;
+
+  return (
+    <div>
+      <h1>Hello</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
 }

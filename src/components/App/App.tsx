331121986e5a3f76e0d692205b5fd8cbc59fe0b4
Maksim <70702008m@gmail.com>
2026-05-13 21:57:48 +0300

    text

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,31 +3,16 @@ import Nature from "../Nature"
 
 const natureImages = [
   {
-    title: "Mountain",
+    title: "Емілія",
     image: "imagess/1.jpg"
-      // "https://images.unsplash.com/photo-1501785888041-af3ef285b470?q=80&w=1200",
+     
   },
-  {
-    title: "Forest",
-    image:
-      "imagess/1.jpg"
-  },
-  {
-    title: "Lake",
-    image:
-     "imagess/2.jpg"
-  },
-  {
+      {
     title: "Ocean",
     image:
       "imagess/3.jpg"
   },
-  {
-    title: "Waterfall",
-    image:
-     "imagess/4.jpg"
-  },
-  {
+    {
     title: "Waterfall",
     image:
      "imagess/5.jpg"
@@ -62,21 +47,12 @@ const natureImages = [
     image:
      "imagess/11.jpg"
   },
-  {
-    title: "Waterfall",
-    image:
-     "imagess/12.jpg"
-  },
-  {
+    {
     title: "Waterfall",
     image:
      "imagess/13.jpg"
   },
-  {
-    title: "Waterfall",
-    image:
-     "imagess/14.jpg"
-  },
+  
 ];
 
 
